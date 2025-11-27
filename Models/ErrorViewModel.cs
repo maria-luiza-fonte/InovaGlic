44f3772e3ab53d1de3d178a70e6a174e7305b5d0
maria-luiza-fonte <marialuizafonte946@gmail.com>
2025-11-27 11:59:08 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InovaGlic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
